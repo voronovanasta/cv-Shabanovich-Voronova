3c08ce55c1716d029f247e93fee6d9bdb1493d70
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2025-07-18 13:37:35 +0300

    feat: add i18n with en and ru languages

--- src/widgets/auth-layout/ui.tsx
+++ src/widgets/auth-layout/ui.tsx
@@ -1,8 +1,10 @@
 import { Box, Container, Tab, Tabs } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { Link, Outlet, useLocation } from 'react-router';
 
 export default function AuthLayout() {
   const location = useLocation();
+  const { t } = useTranslation();
   const currentTab = location.pathname.includes('/register') ? 1 : 0;
   return (
     <Box
@@ -30,8 +32,8 @@ export default function AuthLayout() {
             mb: 3,
           }}
         >
-          <Tab label='LOG IN' component={Link} to='/auth/login' />
-          <Tab label='SIGN UP' component={Link} to='/auth/register' />
+          <Tab label={t('auth.login.tab')} component={Link} to='/auth/login' />
+          <Tab label={t('auth.register.tab')} component={Link} to='/auth/register' />
         </Tabs>
         <Box
           sx={{
